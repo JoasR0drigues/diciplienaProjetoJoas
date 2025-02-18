1df883d4b125f5803cb15057fbc776c7636aa18b
JoasR0drigues <guugueira@gmail.com>
2025-02-17 21:15:54 -0300

    commit inicial

---
+++ src/test/java/br/ueg/desenvolvimento/web/projetoJoas/ProjetoJoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.desenvolvimento.web.projetoJoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
